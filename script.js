71ac25a9421c8f4c6ebde5b2465a5e767e8f53f7
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-05-22 20:08:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function calculateMinCost() {
-  //your code here
-  let input = document.getElementById("rope-lengths").value;
-	let arr = input.split(',');
-	let ans = [];
-	arr.forEach(element => {
-		if(element!=','){
-			ans.push(Number(element));
-		}
-	})
-	let result = [];
-	let sum = 0;
-
-	for (let i = 1; i < ans.length; i++) {
-		ans.sort((a,b)=>a-b);
-		result[i-1] = ans[i]+ans[i-1];
-		ans[i] = ans[i]+ans[i-1];
-	}
-	sum = result.reduce((total,current )=>{
-		return total + current;
-	},0)
+const result = document.getElementById('result');
+const arr = document.getElementById('rope-length').split(',');
 	
-  document.getElementById("result").innerHTML = sum;
+function calculateMinCost() {
   
+  const minHeap = new PriorityQueue();
+	for(const len of arr){
+		minHeap.add(len);
+	}
+
+	let cost = 0;
+
+	while(minHeap.size() > 1) {
+		const min1 = minHeap.poll();
+		const min2 = minHeap.poll();
+
+		const sum = min1 + min2;
+
+		minHeap.add(sum);
+
+		cost += sum;
+	}
+	
+	result.innerText = cost;
 }  
